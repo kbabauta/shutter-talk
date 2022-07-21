780307babf0e505e323972c8490d58109fb8b1d6
Kevin Babauta <kjr.babauta@gmail.com>
2022-07-21 11:39:19 -0700

    heroku troubleshooting

--- server.js
+++ server.js
@@ -32,11 +32,11 @@ app.use((err, req, res, next)=> {
 })
 
 //Heroku Middleware
-app.use(express.static(path.join(__dirname, "client", "build", "index.html")))
+app.use(express.static(path.join(__dirname, "client", "public", "build", "index.html")))
 
 
 app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "client", "build", "index.html"))
+    res.sendFile(path.join(__dirname, "client", "public", "build", "index.html"))
 })
 
 
